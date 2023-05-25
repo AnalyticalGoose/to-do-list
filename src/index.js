0d67894f154cfb1f754c03ecdbf2c93507852127
Harry <harryedgar@hotmail.co.uk>
2023-05-25 16:43:46 +0100

    add avent listerners, functionality to add folders & a modal window

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log("testing123")
+import storageAvailable from "./storage"
+
+const addFolderBtn = document.querySelector('.add-folder-container') 
+const inputContainer = document.querySelector('.input-container')
+const cancelFolderBtn = document.querySelector('.button-cancel')
+const newTaskBtn = document.querySelector('.add-task-button')
+const closeModalBtn = document.querySelector('.close-modal')
+const modal = document.querySelector('.modal')
+
+
+addFolderBtn.addEventListener('click', function() {
+    addFolderBtn.setAttribute('id', 'hide')
+    inputContainer.removeAttribute('id', 'hide')
+})
+
+cancelFolderBtn.addEventListener('click', function() {
+    addFolderBtn.removeAttribute('id', 'hide')
+    inputContainer.setAttribute('id', 'hide')
+})
+
+newTaskBtn.addEventListener('click', function() {
+    modal.showModal();
+})
+
+closeModalBtn.addEventListener('click', function() {
+    modal.close();
+})
+
+
+
+console.log(storageAvailable())
